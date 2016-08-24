d7559c2ad41101277819678f699f72c3dd711909
Liutong Zhou <liutong.zhou@columbia.edu>
2016-08-24 15:43:38 -0400

    Add comments to code for easy understanding
    Add more comments to code for enhancing comprehensability.

--- House-Repairing/MinTAllocation.m
+++ House-Repairing/MinTAllocation.m
@@ -10,22 +10,21 @@ n=length(D);r=length(dt);
 S0=ones(n,r+1);%the start point of S
 lb=zeros(size(S0));%lower bound of S
 ub=repmat(M',1,r+1);%upper bound of S as shown in constraint 1
-A=ones(1,n);A=kron(eye(r+1),sparse(A));
+A=ones(1,n);A=kron(eye(r+1),sparse(A));%inequality constraint A*S<=b as shown in constraint 1
 b=X*ones(1,r+1);%inequality constraint A*S<=b as shown in constraint 1
-%con2 is a function that sets equality constraint as shown in constraint 2
 list=find(index>1);% construct a list of row_index of S which corresponds to the delayed row_index-th task by R
 for i=list
     m=sum(index(i)-1);
 end
-sub=zeros(m,2);z=length(list);j=1;
-for i=1:z
+sub=zeros(m,2);z=length(list);j=1;% 初始化sub用于存储constraint 2 中S的下标 
+for i=1:z % sub 存储了 constraint 2 中S的下标 
     sub(j:j+index(list(i))-1-1,2)=1:index(list(i))-1;
     sub(j:j+index(list(i))-1-1,1)=list(i);
     j=j+index(list(i))-1;
-end% sub 存储了 constraint 2 中S的下标 
-j=sub2ind(size(S0), sub(:,1), sub(:,2));
-Aeq=sparse(1:m,j,ones(m,1),m,n*(r+1));
+end
+j=sub2ind(size(S0), sub(:,1), sub(:,2));%下标转化为线性索引
+Aeq=sparse(1:m,j,ones(m,1),m,n*(r+1));%构建线性等式约束constriant 2 中的Aeq
 beq=sparse(m,1);
-[S,~,deltaT]=fminimax(maxgoal,S0,A,b',Aeq,beq,lb,ub);
-TotalTime=sum([dt,deltaT]);
+[S,~,deltaT]=fminimax(maxgoal,S0,A,b',Aeq,beq,lb,ub);%用优化求解器求解模型
+TotalTime=sum([dt,deltaT]);%输出结果
 end
--- House-Repairing/MinTAllocation2.m
+++ House-Repairing/MinTAllocation2.m
@@ -3,28 +3,27 @@ function [S,TotalTime]= MinTAllocation2(X,D,M,R)
 % Input parameters: X is total labor, D is a demand row vector,
 % M is a labor capacity constarint row vector, R is a ready row vector.
 % Output Parameters: [S,TotalTime]
-[u,~,index]=unique([R,0]);index(end)=[];
+[u,~,index]=unique([R,0]);index(end)=[];%u 和index 同解题思路中的解释一样
 dt=diff(u);%每一个阶段的工时
 n=length(D);r=length(dt);
-S=zeros(n,r+1);D=D';M=M';
-for i=1:r
+S=zeros(n,r+1);D=D';M=M';%初始化人力分配矩阵S
+for i=1:r%对于前r个有预热的阶段
     ind=D>0&index<=i;%ind 为可以动工的子任务的索引
-    S(ind,i)=D(ind)/sum(D(ind))*X;
-    ind2=S(:,i)>M;%超出部分的索引
-    while any(ind2)&&any(ind&S(:,i)<M)%若有超出M且可以把超出部分进行调度到未超出部分
+    S(ind,i)=D(ind)/sum(D(ind))*X;%把labor按比例分配到可以动工的任务中去
+    ind2=S(:,i)>M;%人力分配超出capacity约束部分的索引
+    while any(ind2)&&any(ind&S(:,i)<M)%若有超出M的人力且存在人手未满的任务
     residual=sum(S(ind2,i)-M(ind2));%剩余劳动力
     S(ind2,i)=M(ind2);%重置超出部分满足capacity约束
     ind3=ind&S(:,i)<M;%ind3为 可以动工的人员未满部分的索引
     S(ind3,i)=S(ind3,i)+D(ind3)/sum(D(ind3))*residual;%剩余劳力分配到人员未满部分
-    ind2=S(:,i)>M;%超出部分的索引
-    end
-    %若有超出部分，但所有任务人手已满，则重置，使满足M约束
-    S(ind2,i)=M(ind2);
-    D=D-S(:,i)*dt(i);
+    ind2=S(:,i)>M;%更新超出capacity部分的索引
+    end%迭代至所有任务不超出capacity或者有超出 但所有任务人手已满，则分配结束
+    S(ind2,i)=M(ind2);    %若有超出部分，但所有任务人手已满，则重置，使满足M约束
+    D=D-S(:,i)*dt(i);%进行做工
     D(D<0)=0;
 end
 S(:,r+1)=D/sum(D)*X;%若不超过M
 S(S(:,r+1)>M,r+1)=M(S(:,r+1)>M); %若超过M
-deltaT=max(D./S(:,r+1));
-TotalTime=sum([dt,deltaT]);
+deltaT=max(D./S(:,r+1));%计算最后一个阶段的最小用时
+TotalTime=sum([dt,deltaT]);%计算总项目用时
 end
